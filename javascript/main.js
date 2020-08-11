640417d763bd32dfa420f456ec3cae4c3169d9ef
pulgaresking <newbiepaul24@gmail.com>
2020-08-11 10:44:36 +0800

    animation to the general page added

--- javascript/main.js
+++ javascript/main.js
@@ -9,3 +9,57 @@ button.addEventListener("click", function () {
     links.classList.toggle("linksq");
   }
 });
+
+function showAnimation() {
+  const screenPosition = window.innerHeight / 1.5;
+  const leftContents = document.querySelectorAll(".left-content");
+  const rightContents = document.querySelectorAll(".right-wrapper");
+  const rightImage = document.querySelectorAll(".right-image");
+  const leftImage = document.querySelectorAll(".left-image");
+
+  leftContents.forEach(function (leftContent) {
+    const leftContentsPosition = leftContent.getBoundingClientRect().top;
+
+    if (leftContentsPosition < screenPosition) {
+      leftContent.classList.add("animation-appear");
+    }
+  });
+
+  leftImage.forEach(function (leftImg) {
+    const leftImagePosition = leftImg.getBoundingClientRect().top;
+
+    if (leftImagePosition < screenPosition) {
+      leftImg.classList.add("animation-appear");
+    }
+  });
+
+  rightContents.forEach(function (rightContent) {
+    const rightContentsPosition = rightContent.getBoundingClientRect().top;
+
+    if (rightContentsPosition < screenPosition) {
+      rightContent.classList.add("animation-appear");
+    }
+  });
+
+  rightImage.forEach(function (rightImg) {
+    const rightImagePosition = rightImg.getBoundingClientRect().top;
+
+    if (rightImagePosition < screenPosition) {
+      rightImg.classList.add("animation-appear");
+    }
+  });
+}
+window.addEventListener("scroll", showAnimation);
+
+function introAnimation() {
+  const title = document.querySelector(".intro-title");
+  const subTitle = document.querySelector(".intro-subtitle");
+  const links = document.querySelector(".intro-links");
+  const btn = document.querySelector(".intro-btn");
+
+  title.classList.add("intro-appear");
+  subTitle.classList.add("intro-appear");
+  links.classList.add("intro-appear");
+  btn.classList.add("intro-appear");
+}
+introAnimation();
